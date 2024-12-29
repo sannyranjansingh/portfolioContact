8643edc16576233b63a49a390a8826451de6664e
Sanny <sannyranjansingh8340227316@gmail.com>
2024-12-29 18:07:43 +0530

    fourt commit

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const express = require('express');
 const cors = require("cors");
 const bodyParser = require('body-parser');
 const contactRoutes = require('./view/contactRoutes');
-require('./database/db')
+require('./database/db');
 const app = express();
 
 // Middleware
 app.use(bodyParser.json());
 app.use(cors({
-  origin: "https://sanny-ranjan-singh.github.io", // Your frontend URL
-  methods: 'GET,POST', // Specify allowed methods
-  allowedHeaders: 'Content-Type,Authorization',
+  origin: "https://sanny-ranjan-singh.github.io/portfolio/", // Your frontend URL
+  methods: ['GET', 'POST'], // Allowed methods
+  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+  credentials: true, // Allow cookies if needed
 }));
-
 
 app.use('/api/contact', contactRoutes);
 
